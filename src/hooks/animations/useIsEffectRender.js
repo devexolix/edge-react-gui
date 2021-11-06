103e440ae784cb9784899cccaaa89360efc28abb
Jon Tzeng <jon@edge.app>
2021-11-05 17:04:51 -0700

    WIP Ported from PR #2727: ControlPanel, AccountSwitcher added. Fade and dropdown animations

--- src/hooks/animations/useIsEffectRender.js
+++ src/hooks/animations/useIsEffectRender.js
@@ -2,12 +2,12 @@
 
 import { useEffect, useState } from '../../types/reactHooks'
 
-export const useIsEffectRender = (isRunEffect: boolean, duration: number) => {
+export const useIsEffectRender = (isRunEffect: boolean, druration: number) => {
   const [isRender, setIsRender] = useState(true)
 
   useEffect(() => {
-    isRunEffect ? setIsRender(true) : setTimeout(() => setIsRender(false), duration)
-  }, [isRunEffect, duration])
+    isRunEffect ? setIsRender(true) : setTimeout(() => setIsRender(false), druration + 100)
+  }, [isRunEffect, druration])
 
   return { isRender, setIsRender }
 }
